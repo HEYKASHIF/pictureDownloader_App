895e299d2f10a96d55ad5dd38800e7d72b418eae
HEYKASHIF <kashifmohd269@gmail.com>
2023-11-22 02:41:27 +0530

    commit 3

--- lib/main.dart
+++ lib/main.dart
@@ -28,8 +28,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 2, 25, 140)),
+        // useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Home'),
     );
