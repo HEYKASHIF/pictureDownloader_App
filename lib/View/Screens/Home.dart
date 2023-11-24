2d817a55de4c2feed67ec0b873fce31022843342
HEYKASHIF <kashifmohd269@gmail.com>
2023-11-24 20:19:02 +0530

    commit 4

---
+++ lib/View/Screens/Home.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("HOME"),
+      ),
+    );
+  }
+}
