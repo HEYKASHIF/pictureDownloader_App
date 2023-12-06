bcc9ffcf0f043d752baa121eb0228986d521f88e
HEYKASHIF <kashifmohd269@gmail.com>
2023-12-06 21:56:55 +0530

    commit restarted 3

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
+      // debugShowCheckedModeBanner: false,
       title: 'Picture Downloader',
       home: CategoryScreen(),
     );
