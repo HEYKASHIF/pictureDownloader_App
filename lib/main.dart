bcffb05841da8400464d31b09b1eca0b8d9c34ab
HEYKASHIF <kashifmohd269@gmail.com>
2023-12-16 22:53:14 +0530

    commit restarted 5

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:pictute_downloader/View/Screens/Category.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const MyApp();
+  }
 }
 
 class MyApp extends StatelessWidget {
